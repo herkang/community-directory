83e79fa67fa9ee720d0e5a476a7d57d908c2ab92
Kang Her <her.kang@yahoo.com>
2020-11-07 02:53:14 +0000

    Creates user model and connect to database

--- model.py
+++ model.py
@@ -1,11 +1,40 @@
-""" Models for minority community resource directory"""
+""" Models for minority community resource directory """
 
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///directory'
-app.config['SQLALCHEMY_ECHO'] = True
-app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
+db = SQLAlchemy()
 
-db = SQLAlchemy(app)
+class User(db.Model):
+    """ Creates an User object """
 
+    __tablename__ = 'users'
+
+    user_id = db.Column(db.Integer, 
+                        primary_key=True, 
+                        autoincrement=True)
+    username = db.Column(db.String, nullable=False, unique= True)
+    password = db.Column(db.String)
+
+    def __repr__(self):
+        """ Show user informations """
+        
+        return f'<User user_id={self.user_id} username={self.username}>'
+
+def connect_to_db(flask_app, db_uri='postgresql:///directory', echo=True):
+    flask_app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+    flask_app.config['SQLALCHEMY_ECHO'] = echo
+    flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.app = flask_app
+    db.init_app(flask_app)
+
+    print('Connected to db!')
+
+if __name__ == '__main__':
+    from server import app
+
+    # Call connect_to_db(app, echo=False) if your program output gets
+    # too annoying; this will tell SQLAlchemy not to print out every
+    # query it executes.
+
+    connect_to_db(app)
