54bdd8cc219fac6f2916636ba952140ce62b3d84
Kang Her <her.kang@yahoo.com>
2020-11-11 05:07:11 +0000

    Corrects and adds testing, needs to fix relationship

--- model.py
+++ model.py
@@ -16,25 +16,39 @@ class User(db.Model):
     username = db.Column(db.String, nullable=False, unique= True)
     password = db.Column(db.String)
 
+    #db.create_all() 
+    #test_user = User(username='herrikang', password='test')
+    #db.session.add(test_user)
+    #db.session.commit()
+    #user = User.query.first()
+    #user
+
     def __repr__(self):
         """ Show user informations """
         
         return f'<User user_id={self.user_id} username={self.username}>'
 
-class Resource_category(db.Model):
+class Res_cat(db.Model):
     """ Creates a resource category object """
 
     __tablename__ = 'resource_categories'
 
-    resource_category_id = db.Column(db.Integer, 
+    res_cat_id = db.Column(db.Integer, 
                         primary_key=True, 
                         autoincrement=True)
-    resource_category_name = db.Column(db.String)
+    res_cat_name = db.Column(db.String)
 
     def __repr__(self):
         """ Show user informations """
         
-        return f'<Resource Category resource_category_id={self.resource_category_id} Resource category name resource_category_name={self.resource_category_name}>'
+        return f'<Resource Category res_cat_id={self.res_cat_id} Resource category name res_cat_name={self.res_cat_name}>'
+
+    #db.create_all() 
+    #test_resource = Res_cat(res_cat_name='help')
+    #db.session.add(test_resource)
+    #db.session.commit()
+    #res_cat = Res_cat.query.first()
+    #res_cat
 
 class Resource(db.Model):
     """ Creates a resource object """
@@ -44,38 +58,36 @@ class Resource(db.Model):
     resource_id = db.Column(db.Integer, 
                         primary_key=True, 
                         autoincrement=True)
-    resource_category_id = db.Column(db.Integer, 
-                            db.ForeignKey('users.user_id'))
+    res_cat_id = db.Column(db.Integer, 
+                            db.ForeignKey('resource_categories.res_cat_name'))
     resource_name = db.Column(db.String)
-    # To do, add url to resource_name 
-    # https://sqlalchemy-utils.readthedocs.io/en/latest/data_types.html#module-sqlalchemy_utils.types.phone_number
-
-    service = db.Column(db.String)
-    _phone_number = db.Column(db.Unicode(20))
-    country_code = db.Column(db.Unicode(8))
-
-    phone_number = db.orm.composite(
-        PhoneNumber, 
-        _phone_number, 
-        country_code
-    )
-
-#Testing 
-#user = User(phone_number=PhoneNumber('0401234567', 'FI'))
-
-#user.phone_number.e164  # u'+358401234567'
-#user.phone_number.international  # u'+358 40 1234567'
-#user.phone_number.national  # u'040 1234567'
-#user.country_code  # 'FI'
+    phone_number = db.Column(db.String)
     location = db.Column(db.String)
-    #To do, figure out how to put location in from txt file
     
-    resource = db.relationship('Resource_category', backref='resource_categories')
+    resource = db.relationship('Res_cat', backref='resource_categories')
+
+    #db.create_all() 
+    #test_res_cat = Resource(res_cat_id=test_resource, resource_name='mental', phone_number='651-234-5423', location='MN')
+    #db.session.add(test_res_cat )
+    #db.session.commit()
+    #test_res_cat = Resource.query.first()
+    #test_res_cat
 
     def __repr__(self):
         """ Show user information """
         
         return f'<Resource resource_id={self.resource_id} resource name resource_name={self.resource_name}>'
+
+
+def connect_to_db(flask_app, db_uri='postgresql:///directs', echo=True):
+    flask_app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+    flask_app.config['SQLALCHEMY_ECHO'] = echo
+    flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.app = flask_app
+    db.init_app(flask_app)
+
+    print('Connected to the db!')
 
 if __name__ == '__main__':
     from server import app
