b4f9293b17f2ba5f3abb8a3599c239b907c39bbc
Kang Her <her.kang@yahoo.com>
2020-11-12 04:24:23 +0000

    Adds more crud operations: get_user, creates_res_cat, and get_res_cat

--- crud.py
+++ crud.py
@@ -3,18 +3,36 @@
 from model import db, User, Res_cat, Resource, connect_to_db
 
 def create_user(username, password):
-    user = User(username=username, password=password)
+    """ Creates a user object """
 
+    user = User(username=username, password=password)
     db.session.add(user)
     db.session.commit()
+
     return user
 
-def create_res_cat(res_cat_name):
-#TODO: needs to check on create_res_cat fucntion
+def get_user(user_id):
+    """ Returns user's information by user's id """
+
+    user = User.query.get(user_id)
+
+    return user
+
+def creates_res_cat(res_cat_name):
+    """ Creates a resource category object """
+
     res_cat_name = Res_cat(res_cat_name=res_cat_name)
-    db.session.add(res_cat)
+    db.session.add(res_cat_name)
     db.session.commit()
+
     return res_cat_name 
+
+def get_res_cat(res_cat_id):
+    """ Returns a resource category by id""" 
+
+    res_cat = Res_cat.query.get(res_cat_id)
+
+    return res_cat
 
 if __name__ == '__main__':
     from server import app
