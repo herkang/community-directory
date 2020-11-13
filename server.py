3251f6b4bbaef7d033d05bbce417d8afdd36c898
Kang Her <her.kang@yahoo.com>
2020-11-13 07:35:43 +0000

    Add routes

--- server.py
+++ server.py
@@ -1,12 +1,31 @@
 """ Server for directories """
 
-from flask import Flask
+from flask import (Flask, render_template, request, flash, session,
+                   redirect)
+from model import connect_to_db
+import crud
 
 app = Flask(__name__)
 
 
-# Replace this with routes and view functions!
+@app.route('/')
+def homepage():
+    """ View homepage """
 
+    return render_template('homepage.html')
 
+@app.route('/users')
+def users():
+    """ View user """
+
+    return render_template('user_details.html')
+
+@app.route('/categories')
+def categories():
+    """ View categories """
+
+    return render_template('category_details.html')
+
+@app.
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
