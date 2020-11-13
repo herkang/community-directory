3f65bcc720b2b7cb084dbb97ad9f082e280a72dd
Kang Her <her.kang@yahoo.com>
2020-11-13 14:43:02 +0000

    Updates

--- server.py
+++ server.py
@@ -5,8 +5,11 @@ from flask import (Flask, render_template, request, flash, session,
 from model import connect_to_db
 import crud
 
+from jinja2 import StrictUndefined
 app = Flask(__name__)
 
+app.secret_key = "dev"
+app.jinja_env.undefined = StrictUndefined
 
 @app.route('/')
 def homepage():
@@ -24,8 +27,11 @@ def users():
 def categories():
     """ View categories """
 
-    return render_template('category_details.html')
+    categories = crud.get_category()
+
+    return render_template('category_details.html', categories=categories)
 
 @app.
 if __name__ == '__main__':
+    connect_to_db(app)
     app.run(host='0.0.0.0', debug=True)
