c7507b793207f6dcf3202ecf7b83491d26cce13e
Kang Her <her.kang@yahoo.com>
2020-11-14 04:36:06 +0000

    Attempted with bs2json

--- stcloudstate.py
+++ stcloudstate.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+from bs2json import bs2json
 
 source = requests.get('https://www.stcloudstate.edu/mrc/outreach/mn-orgs.aspx').text
 
@@ -8,24 +9,32 @@ soup = BeautifulSoup(source, 'lxml')
 #whole webpage
 #print(soup.prettify())
 
-table = soup.find('tbody')
-#all table content in html pretiffy and links
-#print(table.prettify())
+converter = bs2json()
 
-summary = table.find_all('tr')
-#similiar to table above, but can't prettify
-#print(summary)
-
-def data(summary):
-    for row in summary:
-
-        return row_data = [td.text for td in row.find_all('td')]
+tag = soup.find('td').text
+json = converter.convert(tag)
+print(json)
 
 
-resource_obj = table.tr.text
-#one resource object
-#print(resource_obj)
 
-category = soup.find('tbody').td.text
-#print one race category
-#print(category)
+# table = soup.find('tbody')
+# #all table content in html pretiffy and links
+# #print(table.prettify())
+
+# summary = table.find_all('tr')
+# #similiar to table above, but can't prettify
+# #print(summary)
+
+# def data(summary):
+#     for row in summary:
+#         row_data = [td.text for td in row.find_all('td')]
+
+#         print(row_data)
+
+# resource_obj = table.tr.text
+# #one resource object
+# #print(resource_obj)
+
+# category = soup.find('tbody').td.text
+# #print one race category
+# #print(category)
