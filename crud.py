e1fdda2a2a2ae03c3bf15ccbd33db8daea4ea281
Kang Her <her.kang@yahoo.com>
2020-11-17 05:21:00 +0000

    Update functions and provided exact return

--- crud.py
+++ crud.py
@@ -2,69 +2,87 @@
 
 from model import db, User, Category, Resource, UserResource, connect_to_db
 
+"""
+For all user related functions, returns will be something like:
+ <ID=1 Username=Jet>
+"""
 def create_user(username, password):
-    """ Creates and return new user """
+    """ Create and return a new user instance """
 
-    user = User(username=username, password=password)
+    new_user = User(username=username, password=password)
     
-    db.session.add(user)
+    db.session.add(new_user)
     db.session.commit()
 
-    return user
+    return new_user
 
-def get_user():
+def get_user(id):
     """ Return user by primary key """
 
-    return User.query.get(user_id)
-
+    return User.query.filter(User.id==id).first()
+    
 def get_user_by_username(username):
     """ Return user by username """
 
-    return User.query.filter(User.username == username).first()
-
+    return User.query.filter(User.username==username).first()
 
 def create_category(category):
-    """ Create a category instance """
+    """ Create and return a new category instance """
 
-    category = Category(category=category)
-    db.session.add(category)
+    new_category = Category(category=category)
+
+    db.session.add(new_category)
     db.session.commit()
 
-    return category
+    return new_category
 
 def get_category():
-    """ Return resource categories """ 
+    """ Returns all categories in a list:
+        [<ID=1 Category=Africans>, <ID=2 Category=American Indian>]
+    """ 
 
     return Category.query.all()
 
-def create_resource(category, resource, phone_number, location):
-    """ Creates a resource object"""
-   
-    resource = Resource(category=category, resource=resource, phone_number=phone_number, location=location)
-    db.session.add(resource)
+#TODO: Add category relationship, resource functions not properly working
+
+
+def create_resource(resource, phone_number, location):
+    """ Create and return a new resource instance 
+    Will return: 'Confederation of Somali'
+    """
+    
+    new_resource = Resource(resource=resource, phone_number=phone_number, location=location)
+    db.session.add(new_resource)
     db.session.commit()  
 
     return resource
 
-def get_resource():
-    """ Return all resources """
+""" 
+BOTH get_all_resources and get_resource_by_id will return:
+ [<ID=1 Category ID=None Resource=Confederation of Somali>]
+"""
+
+def get_all_resources():
+    """ Returns all resources """
 
     return Resource.query.all()
 
-def add_bookmark_by_id(resource_id):
-    """ Creates an instance of bookmark resource"""
+def get_resource_by_id(id):
+    """ Return resource by resource id """
 
-    bookmarks = User_resource(resource_id=resource_id)
-    db.session.add(bookmarks)
-    db.session.commit()
+    return Resource.query.filter(Resource.id==id).first()
 
-    return bookmarks
+# def add_bookmark_by_id(resource_id):
+#     """ Create and return a bookmark resource instance """
 
-def get_bookmarK_by_user_id(user_id):
-    """ Return user's favorite """
+#     new_bookmarks = UserResource(resource_id=resource_id)
+#     db.session.add(new_bookmarks)
+#     db.session.commit()
 
-    #TODO: Get resource and create a list for favorites
+#     return new_bookmarks
 
+# def get_bookmarks_by_user_id(user_id):
+#     """ Return user's favorite """
 
 if __name__ == '__main__':
     from server import app
