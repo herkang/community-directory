7c128bd687d17948420f16194eada2af337d9cbc
Kang Her <her.kang@yahoo.com>
2020-11-23 13:15:25 +0000

    Upater with server side routes

--- server.py
+++ server.py
@@ -30,7 +30,7 @@ def load_user(user_id):
 def homepage():
     """View homepage"""
 
-    categories = crud.get_category()
+    categories = crud.get_categories()
 
     return render_template('homepage.html', categories=categories)
 
@@ -57,11 +57,6 @@ def register_user():
         flash('Account created! Please log in.')    
         return redirect('/login')
 
-@app.route('/login')
-def login_form():
-
-    return render_template('login.html')
-
 @app.route('/login', methods=['POST'])
 def login():
 
@@ -69,83 +64,42 @@ def login():
     password = request.form.get('password')
 
     user = crud.get_user_by_username(username)
-
     if user:
         # Check password, if password matches, call login_user
         if password == user.password:
             login_user(user)
-            
             flash('Welcome back!')
             print('***********', user.id, 'in terminal***********')
-            
             next_ = request.args.get('next')
-          
             if not is_safe_url(next_):
                 return abort(400)
             return redirect(next_ or '/profile' )
-
         else:
-            flash('Wrong password, try again.')
-            
+            flash('Wrong password, try again.') 
     else:
         # Tell user that the account doesn't exist
         flash("User doesn't exit, please sign up!")
-
     return redirect('/')
+    
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect('/') 
 
 @app.route('/profile')
 @login_required
 def profile():
     """Return user profile and saved resources"""
 
-    return render_template('profile.html', bookmarks=[])
+    return render_template('profile.html')
 
-@app.route('/logout')
+@app.route('/profile', methods=['POST'])
 @login_required
-def logout():
-    logout_user()
-    return redirect('/categories') 
+def add_bookmark(resource):
+    """Return user profile and saved resources"""
 
-@app.route('/categories', methods=['POST'])
-def create_category(category):
-
-    return crud.create_category(category)
-
-@app.route('/categories') #already shown in hompage
-def categories():
-    """View all categories"""
-    # categories = crud.get_category()
-    # return render_template('all_categories.html', categories=categories)
-    return redirect('/')
-
-@app.route('/categories/<category_id>')
-def category(category_id):
-
-    category = crud.get_category_by_id(category_id)
-
-    return render_template('category.html', category=category)
-
-@app.route('/resources', methods=['POST'])
-def create_resource(resource, contact, location, category):
-
-    return crud.create_resource(resource, contact, location, category)
-
-@app.route('/resources')
-def all_resources():
-    """View all resources in category"""
-
-    resources = crud.get_all_resources()
-
-    return render_template('all_resources.html', resources=resources)
-
-@app.route('/resources/<category_id>')
-def resource(resource_id):
-    """View selected category resource"""
-
-    resource = crud.get_resource_by_id(resource_id)
-
-    return render_template('resources.html', resource=resource)
-########## Create Bookmark ##########
+    return crud.add_to_bookmark_list(resource)
 
 @app.route('/profile/<user_id>')
 @login_required
@@ -156,31 +110,57 @@ def user_resource(user_id):
 
     return render_template('profile.html', bookmarks=bookmarks)
 
-@app.route("/bookmark-info.json")
-def bookmark_info():
-    """Return info about a bookmark as JSON."""
+@app.route('/categories', methods=['POST'])
+def create_category(category):
 
-    # In real life, we would probably get this info
-    # from our database
-    bookmark = {
-        "id": "1",
-        "username": "test1",
-        "bookmark": ["US", "CA", "MX"]
-    }
+    return crud.create_category(category) #create data in server terminal
 
-    return jsonify(bookmark)
+@app.route('/categories') 
+def categories():
+    """View all categories"""
+    # categories = crud.get_category()
+    # return render_template('all_categories.html', categories=categories)
+    return redirect('/') #redirect to homepage
 
-@app.route('/resources', methods=['POST'])
+@app.route('/categories/<category_id>')
+def category(category_id):
+
+    category = crud.get_category_by_id(category_id)
+
+    return render_template('category.html', category=category)
+
+#create on server side
+# @app.route('/resources', methods=['POST'])
+# def create_resource(resource, contact, location, category):
+
+#     return crud.create_resource(resource, contact, location, category)
+
+@app.route('/resources')
+def all_resources():
+    """View all resources in category"""
+
+    resources = crud.get_all_resources()
+
+    return render_template('all_resources.html', resources=resources)
+
+@app.route('/resources', methods=['POST']) #Method Not Allowed
 @login_required
 def add_resource():
 
-    user_resource = request.form.get('bookmark')
+    bookmarks = request.form.get('bookmark')
 
     flash('Your resource has been bookmarked!')
 
-    bookmark= crud.add_bookmark(user.id, resource)
+    # return redirect('/resources')
+    return render_template('/profile.html', bookmarks=bookmarks)
 
-    return redirect('/resources') 
+@app.route('/resources/<category_id>')
+def resource(category_id):
+    """View selected category resource"""
+
+    resource = crud.get_resource_by_id(category_id)
+
+    return render_template('resources.html', resource=resource)
 
 if __name__ == '__main__':
     connect_to_db(app)
