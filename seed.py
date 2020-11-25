bfeecf954b61c6793090759a7d6de956db303bd3
Kang Her <her.kang@yahoo.com>
2020-11-25 21:26:32 +0000

    New way to seed data

--- seed.py
+++ seed.py
@@ -10,30 +10,59 @@ os.system('dropdb directory')
 os.system('createdb directory')
 
 model.connect_to_db(server.app)
+model.db.drop_all()
 model.db.create_all()
 
 with open('db.csv', 'r') as data:
 
     for line in csv.DictReader(data):
-        for k, v in line.items():
+
+        category = None
+        org = None
+        contact = None
+        address = None
+        location = None
+
+        for key, val in line.items():
             # print(line)
-            # print({k: v})
-            category = crud.get_category_by_name(line['Category'])
-            if not category:
-                item = crud.create_category(line['Category'])
+            print({key: val})
+            print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+       
+            if key == "Category":
+
+                category = crud.get_category_by_name(line['Category'])
+
+                if category == None:
+                    #TODO create 
+                    new_category = create_category
+
+            if key == "Org Name":
+
+            
+            if key == "Contact":
+
+            if key == "Address":
+
+            if key == "Location":
+
+
+
+            # 
+            # if not category:
+            #     item = crud.create_category(line['Category'])
                 # item = crud.get_category_by_id(line['Category'])
                 # print('!!!!!!!!', line['Category'],'!!!!!!!!')
                 # !!!!!!!! Latino/as !!!!!!!!             
                 # print('!!!!!!!!', item.id,'!!!!!!!!')
                 # 5 !!!!!!!!  
-            else:
-                category_id = crud.get_resources_by_category(item.id)
 
-                # if not resource:
-                    # category = line['Category']
-                org_name = line['Org Name']
-                contact = line['Contact']
-                address = line['Address']
-                location = line['Location']
-                resource_item = crud.create_resource(org_name, contact, address, location, item.id)
-
+            # else:
+            #     category_id = crud.get_resources_by_category(item.id)
+                
+            #     # if not resource:
+            #         # category = line['Category']
+            #     org_name = line['Org Name']
+            #     contact = line['Contact']
+            #     address = line['Address']
+            #     location = line['Location']
+            #     resource_item = crud.create_resource(org_name, contact, address, location, item.id)
